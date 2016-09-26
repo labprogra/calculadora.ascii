076766a7451cfc944d971bbdd2aea3512229677c
DannyAndres <danny.aguilar@usach.cl>
2016-09-26 12:21:13 -0300

    matriz de ejemplo

---
+++ numeros/example.py
@@ -0,0 +1,11 @@
+#matriz base para hacer las demas
+
+
+matriz = []
+for i in range(7):
+    matriz.append([])
+    for j in range(5):
+        matriz[i].append(".")
+
+for i in matriz:
+    print(i)

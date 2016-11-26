752a01ec90951126dcd00905fb5131c588360705
dannyandres <danny.aguilar@usach.cl>
2016-11-26 18:46:26 -0300

    test

--- test.py
+++ test.py
@@ -1,14 +1,1 @@
-class Persona:
-	def __init__(self, nombre):
-		self.nombre = nombre
-		self.edad = 20
-		self.colorPelo = "negro"
-
-	def hablar(self):
-		return self.nombre
-
-persona = Persona("danny")
-
-mensaje = objeto.hablar()
-
-print mensaje
+#archivo de testing
